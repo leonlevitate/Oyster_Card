659e9cd5432e39bf4bc4bcd94f623dda6e8b10f7
Leon Linton <leon.linton@live.co.uk>
2019-06-05 19:58:27 +0100

    Add method to record station when touching in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,12 +3,12 @@ class Oystercard
   MAX_BALANCE = 90
   MIN_BALANCE = 1
 
-  attr_reader :balance, :in_use, :MIN_BALANCE, :MAX_BALANCE
+  attr_reader :balance, :in_use, :MIN_BALANCE, :MAX_BALANCE, :entry_station
 
 
   def initialize
     @balance = 0
-    @in_use = false
+    # @in_use = false
   end
 
   def top_up(value)
@@ -17,17 +17,20 @@ class Oystercard
   end
 
   def in_journey?
-    @in_use
+    # @in_use
+    @entry_station
   end
 
-  def touch_in
-    @in_use = true
+  def touch_in(station)
     fail "Your balance is below £1. Please top up" if @balance < MIN_BALANCE
+    @in_use = true
+    @entry_station = station
   end
 
   def touch_out
     deduct(MIN_BALANCE)
     @in_use = false
+    entry_station = nil
   end
 
 private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,8 @@ require 'oystercard'
 describe Oystercard do
   subject(:card) { described_class.new }
 
+  let(:station){ double :station }
+
   it 'has a balance of zero' do
   expect(card.balance).to eq(0)
   end
@@ -19,13 +21,7 @@ describe Oystercard do
       card.top_up(max_balance)
       expect{ card.top_up 1 }.to raise_error ("Maximum balance of #{max_balance} exceeded")
     end
-  # This Test is no longer required:
-  # describe '#deduct' do
-  #   it 'takes an argument and deducts it from the balance' do
-  #   card.top_up(45)
-  #   expect(card.deduct(5)).to eq (40)
-  #   end
-  # end
+
   describe '#in_journey' do
     it 'is initially not in a journey' do
       expect(card.in_journey?).to be_falsey
@@ -33,24 +29,32 @@ describe Oystercard do
   end
 
   describe '#touch_in' do
+
     it 'does not allow touch in if balance is under £1' do
-       expect { card.touch_in }.to raise_error "Your balance is below £1. Please top up"
+       expect { card.touch_in(station) }.to raise_error "Your balance is below £1. Please top up"
     end
 
     it 'allows customer to touch in' do
       min_balance = Oystercard::MIN_BALANCE
       card.top_up(min_balance)
-      card.touch_in
+      card.touch_in(station)
       expect(card).to be_in_journey
     end
 
     it 'reduces balance by minimum fair' do
       min_balance = Oystercard::MIN_BALANCE
       card.top_up(min_balance)
-      card.touch_in
+      card.touch_in(station)
       expect { card.touch_out }.to change{card.balance}.by(-Oystercard::MIN_BALANCE)
     end
   end
+
+    it 'records the entry station' do
+      min_balance = Oystercard::MIN_BALANCE
+      card.top_up(min_balance)
+      card.touch_in(station)
+      expect(card.entry_station).to eq station
+    end
 
   describe '#touch_out' do
     it 'allows customer to touch out' do
